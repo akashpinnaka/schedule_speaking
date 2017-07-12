08eaf2d4d2c0d254892bdbb26870b714b66f1677
akashpinnaka <aakash.pinnaka@gmail.com>
2017-07-12 17:19:39 +0530

    Add active record validations to user model, fill controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,51 @@
 class UsersController < ApplicationController
-  def index
-  end
 
-  def new
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all.where(admin: false)
   end
 
   def show
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      redirect_to user_path, notice: "You have successfully created an account. Hope you have a great learning."
+    else
+      render "new"
+    end
+  end
+
   def edit
+  end
+
+  def update
+    if @user.update user_params
+      redirect_to user_path, notice: "Changes saved successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, notice: "You have successfully removed the user."
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :admin)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, length: { in: 6..15 }
 end
